9de1faa072f0bf35ebb1f29e4bf27fd24ef91074
michaelmegliola <michael.megliola@gmail.com>
2019-03-01 13:49:21 -0500

    corrected degenerate case

--- mountain-car-q-degenerate.py
+++ mountain-car-q-degenerate.py
@@ -34,13 +34,12 @@ for i in range(iters):
         q[ds[0],ds[1],action] = reward+np.max(q[do[0],do[1]])
         ds = do                                 # use obs as next starting state
         if i%1000==0: env.render()
-    explore = max(explore*decay,exp_min)        # decay explore rate
 
     ''' just reporting results from here on '''
     recent[i%100] = max_pos
     if i>=100: max_positions.append(np.mean(recent))
-    if i%1000==0: print('episode {:d}, max pos {:3.2f}, avg max(last 100) {:3.2f}, explore {:1.5f}'.
-                  format(i,max_pos,np.mean(recent),explore))
+    if i%1000==0: print('episode {:d}, max pos {:3.2f}, avg max(last 100) {:3.2f}'.
+                  format(i,max_pos,np.mean(recent)))
 
 plt.plot(max_positions)
 plt.title('episodes = ' + str(iters))
